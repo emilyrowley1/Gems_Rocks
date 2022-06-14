e44c35dc341f821c332887ddf29b39841dfe1a38
Emily Rowley <erowley49@gmail.com>
2022-06-13 23:39:52 -0600

    Finished Game

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Unit04.Game.Casting;
+using Unit04.Game.Directing;
+using Unit04.Game.Services;
+
+
+namespace Unit04
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        private static int FRAME_RATE = 12;
+        private static int MAX_X = 900;
+        private static int MAX_Y = 600;
+        private static int CELL_SIZE = 15;
+        private static int FONT_SIZE = 15;
+        private static int COLS = 60;
+        private static int ROWS = 40;
+        private static string CAPTION = "Rocks and Gems";
+        private static Color WHITE = new Color(255, 255, 255);
+        private static int DEFAULT_ARTIFACTS = 40;
+
+
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            // create the cast
+            Cast cast = new Cast();
+
+            // create the banner
+            Actor banner = new Actor();
+            banner.SetText("");
+            banner.SetFontSize(FONT_SIZE);
+            banner.SetColor(WHITE);
+            banner.SetPosition(new Point(CELL_SIZE, 0));
+            cast.AddActor("banner", banner);
+
+            // create the robot
+            Actor robot = new Actor();
+            robot.SetText("#");
+            robot.SetFontSize(FONT_SIZE);
+            robot.SetColor(WHITE);
+            robot.SetPosition(new Point(MAX_X / 2, 585));
+            cast.AddActor("robot", robot);
+
+            // create the artifacts
+            for (int i = 0; i < DEFAULT_ARTIFACTS; i++)
+            {
+                Program.createNewArtifact(i, cast);
+            }
+            
+            // start the game
+            KeyboardService keyboardService = new KeyboardService(CELL_SIZE);
+            VideoService videoService 
+                = new VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE, false);
+            Director director = new Director(keyboardService, videoService);
+            director.StartGame(cast);
+
+            // test comment
+        }
+
+        public static void createNewArtifact(int rockOrGemDeterminer, Cast cast)
+        {
+            Random random = new Random();
+            Artifact artifact = new Artifact();
+                string text;
+                if (rockOrGemDeterminer % 3 == 0)
+                {
+                    text = "0";
+                    artifact.SetValue(-1);
+                }
+                else
+                {
+                    text = "*";
+                    artifact.SetValue(1);
+                }
+                string message = "This is a message";
+
+                int x = random.Next(1, COLS);
+                int y = random.Next(1, ROWS);
+                int speed = random.Next(3, 7);
+                Point position = new Point(x, y);
+                position = position.Scale(CELL_SIZE);
+
+                int r = random.Next(0, 256);
+                int g = random.Next(0, 256);
+                int b = random.Next(0, 256);
+                Color color = new Color(r, g, b);
+
+                artifact.SetText(text);
+                artifact.SetFontSize(FONT_SIZE);
+                artifact.SetColor(color);
+                artifact.SetPosition(position);
+                artifact.SetMessage(message);
+                artifact.SetVelocity(new Point(0, speed));
+                cast.AddActor("artifacts", artifact);
+        }
+    }
+}
